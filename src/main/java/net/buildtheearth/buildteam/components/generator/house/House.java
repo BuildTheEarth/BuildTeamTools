768aa910dcbc17c8744b1720b997dadc159622bb
Adam <adamwilliams0604@gmail.com>
2022-06-03 17:09:48 +0100

    Updated wiki links

--- src/main/java/net/buildtheearth/buildteam/components/generator/house/House.java
+++ src/main/java/net/buildtheearth/buildteam/components/generator/house/House.java
@@ -1,25 +1,20 @@
 package net.buildtheearth.buildteam.components.generator.house;
 
-import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.regions.RegionOperationException;
 import net.buildtheearth.buildteam.BuildTeam;
 import net.buildtheearth.buildteam.components.generator.Generator;
 import net.buildtheearth.utils.Utils;
 import org.apache.commons.lang.StringUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 public class House {
 
-    public static String WIKI_PAGE = "https://github.com/MineFact/BuildTeamPlugin/blob/master/wiki/generator/houses.md";
-
+    public static String HOUSE_WIKI = "https://github.com/BuildTheEarth/BuildTeamTools/wiki/House-Command";
+    public static String INSTALL_WIKI = "https://github.com/BuildTheEarth/BuildTeamTools/wiki/Installation";
 
 
     public static void analyzeCommand(Player p, String[] args){
@@ -76,7 +71,7 @@ public class House {
     public static void sendMoreInfo(Player p){
            p.sendMessage(" ");
            p.sendMessage("§cFor more information take a look at the wiki:");
-           p.sendMessage("§c" + WIKI_PAGE);
+           p.sendMessage("§c" + HOUSE_WIKI);
     }
 
     public static void sendError(Player p){
@@ -87,7 +82,9 @@ public class House {
         // Check if WorldEdit is enabled
         if(!BuildTeam.DependencyManager.isWorldEditEnabled()){
             p.sendMessage("§cPlease install WorldEdit to use this tool.");
-            sendMoreInfo(p);
+            p.sendMessage(" ");
+            p.sendMessage("§cFor more installation help take a look at the wiki:");
+            p.sendMessage("§c" + INSTALL_WIKI);
         }
 
         // Get WorldEdit selection of player

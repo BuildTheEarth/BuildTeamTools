36a63c72566ced289413db086b5ae9585615d55c
George <georgewalk168@gmail.com>
2023-08-31 22:03:43 +0100

    Moved into new universal_navigator package

--- src/main/java/net/buildtheearth/buildteam/components/universal_experience/universal_navigator/TutorialsMenu.java
+++ src/main/java/net/buildtheearth/buildteam/components/universal_experience/universal_navigator/TutorialsMenu.java
@@ -1,4 +1,4 @@
-package net.buildtheearth.buildteam.components.universal_experience;
+package net.buildtheearth.buildteam.components.universal_experience.universal_navigator;
 
 import net.buildtheearth.Main;
 import net.buildtheearth.utils.MenuItem;

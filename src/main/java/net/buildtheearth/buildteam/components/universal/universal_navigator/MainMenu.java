728135495a0e4395fe95227c909e397a11392059
MineFact <minefact.minecraft@gmail.com>
2023-09-17 19:15:13 +0200

    recoded the MainMenu to get the code working and reduce its complexity

--- src/main/java/net/buildtheearth/buildteam/components/universal/universal_navigator/MainMenu.java
+++ src/main/java/net/buildtheearth/buildteam/components/universal/universal_navigator/MainMenu.java
@@ -1,22 +1,22 @@
 package net.buildtheearth.buildteam.components.universal.universal_navigator;
 
+import com.alpsbte.alpslib.utils.item.ItemBuilder;
+import com.sun.tools.javac.util.List;
 import net.buildtheearth.Main;
-import net.buildtheearth.buildteam.Network;
-import net.buildtheearth.buildteam.components.universal.PreferenceType;
 import net.buildtheearth.utils.Item;
-import net.buildtheearth.utils.MenuItem;
 import net.buildtheearth.utils.Utils;
 import net.buildtheearth.utils.io.ConfigPaths;
 import net.buildtheearth.utils.menus.AbstractMenu;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.ipvp.canvas.mask.BinaryMask;
 import org.ipvp.canvas.mask.Mask;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * The main menu for the BTE universal navigator. <p>
@@ -32,228 +32,163 @@ import java.util.ArrayList;
  */
 public class MainMenu extends AbstractMenu
 {
-    private static final int iRows = 3;
-    private static final String szInventoryName = "Build the Earth";
-    private static final ArrayList<MenuItem> menuItems = getGui(Main.buildTeamTools.getNetwork());
-    private static final FileConfiguration config = Main.instance.getConfig();
+    private static final int rows = 3;
+    private static final String inventoryName = "Build the Earth";
+    private static FileConfiguration config;
 
     public MainMenu(Player menuPlayer)
     {
-        super(iRows, szInventoryName, menuPlayer);
+        super(rows, inventoryName, menuPlayer);
     }
 
-    /**
-     * Produces a list of Menu Items for the MainMenu gui
-     * @see MenuItem
-     * @return
-     */
-    public static ArrayList<MenuItem> getGui(Network network)
-    {
-        //Initiates the list
-        ArrayList<MenuItem> menuItems = new ArrayList<>();
+    @Override
+    protected void setPreviewItems() {
+        config = Main.instance.getConfig();
+        int[] slots = getSlots();
 
-        //Gets the list of items needed in the GUI
-        int iItemsNeeded = 0;
-        boolean[] bItemsNeeded = new boolean[3];
-        String[] szCommands = new String[3];
-        String[] szMessages = new String[3];
-        int[] iSlotsToBeUsed;
+        // Fill the blank slots with glass panes
+        getMenu().getSlot(11).setItem(new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 7).setName(" ").build());
+        getMenu().getSlot(13).setItem(new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 7).setName(" ").build());
+        getMenu().getSlot(15).setItem(new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 7).setName(" ").build());
 
-        bItemsNeeded[0] = config.getBoolean(ConfigPaths.BUILD_ITEM_ENABLED);
-        if (bItemsNeeded[0])
-            iItemsNeeded++;
-        else
-        {
-            szCommands[0] = config.getString(ConfigPaths.BUILD_ITEM_COMMAND);
-            szMessages[0] = config.getString(ConfigPaths.BUILD_ITEM_MESSAGE);
-        }
-        bItemsNeeded[1] = config.getBoolean(ConfigPaths.EXPLORE_ITEM_ENABLED);
-        if (bItemsNeeded[1])
-            iItemsNeeded++;
-        else
-        {
-            szCommands[1] = config.getString(ConfigPaths.BUILD_ITEM_COMMAND);
-            szMessages[1] = config.getString(ConfigPaths.BUILD_ITEM_MESSAGE);
+
+        // Set Build Item
+        if(config.getBoolean(ConfigPaths.BUILD_ITEM_ENABLED)) {
+            ArrayList<String> buildLore = new ArrayList<>(Arrays.asList(Utils.loreText(config.getString(ConfigPaths.BUILD_ITEM_LORE))));
+            ItemStack buildItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.BUILD_ITEM_MATERIAL)), ChatColor.GREEN + "" + ChatColor.BOLD + "Build", 1, buildLore);
+            getMenu().getSlot(slots[0]).setItem(buildItem);
         }
 
-        bItemsNeeded[2] = config.getBoolean(ConfigPaths.TUTORIALS_ITEM_ENABLED);
-        if (bItemsNeeded[2])
-            iItemsNeeded++;
-
-        //Deals with the number of items needed
-        if (iItemsNeeded == 0)
-            return menuItems;
-        else
-            iSlotsToBeUsed = MenuItem.getSlotIndexesMiddleRowOf3(iItemsNeeded);
-
-        //-----------------------------------------------------
-        //--------------Adds the items to the GUI--------------
-        //-----------------------------------------------------
-        int iItem = 0;
-
-        //-------------------------------------------
-        //-------------------Build-------------------
-        //-------------------------------------------
-        if (bItemsNeeded[0])
-        {
-            ArrayList<String> buildLore = new ArrayList<>();
-            buildLore.add(Utils.loreText(config.getString(ConfigPaths.BUILD_ITEM_LORE)));
-
-            ItemStack buildItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.BUILD_ITEM_MATERIAL)), ChatColor.GREEN +"" +ChatColor.BOLD +"Build", 1, buildLore);
-            MenuItem build = new MenuItem(iSlotsToBeUsed[iItem], buildItem, player ->
-            {
-                //Opens the build menu for the player
-                new BuildMenu(player);
-            });
-            menuItems.add(build);
-
-            iItem++;
-        }
-        //A command was specified
-        else if (szCommands[0] != null)
-        {
-            if (szCommands[0].startsWith("/"))
-            {
-                ArrayList<String> buildLore = new ArrayList<>();
-                buildLore.add(Utils.loreText(config.getString(ConfigPaths.BUILD_ITEM_LORE)));
-
-                ItemStack buildItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.BUILD_ITEM_MATERIAL)), ChatColor.GREEN +"" +ChatColor.BOLD +"Build", 1, buildLore);
-                MenuItem build = new MenuItem(iSlotsToBeUsed[iItem], buildItem, player ->
-                {
-                    Bukkit.getScheduler().runTask(Main.instance, () ->
-                    {
-                        //Performs the command
-                        player.performCommand(szCommands[0]);
-                    });
-                });
-                menuItems.add(build);
-
-                iItem++;
-            }
-        }
-        //A message was specified
-        else if (szMessages[0] != null)
-        {
-            ArrayList<String> buildLore = new ArrayList<>();
-            buildLore.add(Utils.loreText(config.getString(ConfigPaths.BUILD_ITEM_LORE)));
-
-            ItemStack buildItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.BUILD_ITEM_MATERIAL)), ChatColor.GREEN +"" +ChatColor.BOLD +"Build", 1, buildLore);
-            MenuItem build = new MenuItem(iSlotsToBeUsed[iItem], buildItem, player ->
-            {
-                //Sends the message
-                player.sendMessage(ChatColor.GREEN +szMessages[0]);
-            });
-            menuItems.add(build);
-
-            iItem++;
+        // Set Explore Item
+        if(config.getBoolean(ConfigPaths.EXPLORE_ITEM_ENABLED)) {
+            ArrayList<String> exploreLore = new ArrayList<>(Arrays.asList(Utils.loreText(config.getString(ConfigPaths.EXPLORE_ITEM_LORE))));
+            ItemStack exploreItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.EXPLORE_ITEM_MATERIAL)), ChatColor.YELLOW + "" + ChatColor.BOLD + "Explore", 1, exploreLore);
+            getMenu().getSlot(slots[1]).setItem(exploreItem);
         }
 
-        //-------------------------------------------
-        //------------------Explore------------------
-        //-------------------------------------------
-        if (bItemsNeeded[1])
-        {
-            ArrayList<String> exploreLore = new ArrayList<>();
-            exploreLore.add(Utils.loreText(config.getString(ConfigPaths.EXPLORE_ITEM_LORE)));
-
-            ItemStack exploreItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.EXPLORE_ITEM_MATERIAL)), ChatColor.YELLOW +"" +ChatColor.BOLD +"Explore", 1, exploreLore);
-            MenuItem explore = new MenuItem(iSlotsToBeUsed[iItem], exploreItem, player ->
-            {
-                //Opens the explore menu for the player
-                new ExploreMenu(player, network.isBTENetworkConnected());
-            });
-            menuItems.add(explore);
-
-            iItem++;
-        }
-        //A command was specified
-        else if (szCommands[1] != null)
-        {
-            if (szCommands[1].startsWith("/"))
-            {
-                ArrayList<String> exploreLore = new ArrayList<>();
-                exploreLore.add(Utils.loreText(config.getString(ConfigPaths.EXPLORE_ITEM_LORE)));
-
-                ItemStack exploreItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.EXPLORE_ITEM_MATERIAL)), ChatColor.YELLOW +"" +ChatColor.BOLD +"Explore", 1, exploreLore);
-                MenuItem explore = new MenuItem(iSlotsToBeUsed[iItem], exploreItem, player ->
-                {
-                    Bukkit.getScheduler().runTask(Main.instance, () ->
-                    {
-                        //Performs the command
-                        player.performCommand(szCommands[1]);
-                    });
-                });
-                menuItems.add(explore);
-
-                iItem++;
-            }
-        }
-        //A message was specified
-        else if (szMessages[1] != null)
-        {
-            ArrayList<String> exploreLore = new ArrayList<>();
-            exploreLore.add(Utils.loreText(config.getString(ConfigPaths.EXPLORE_ITEM_LORE)));
-
-            ItemStack exploreItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.EXPLORE_ITEM_MATERIAL)), ChatColor.YELLOW +"" +ChatColor.BOLD +"Explore", 1, exploreLore);
-            MenuItem explore = new MenuItem(iSlotsToBeUsed[iItem], exploreItem, player ->
-            {
-                //Sends the message
-                player.sendMessage(ChatColor.GREEN +szMessages[1]);
-            });
-            menuItems.add(explore);
-
-            iItem++;
-        }
-
-
-        //-------------------------------------------
-        //-----------------Tutorials-----------------
-        //-------------------------------------------
-        if (bItemsNeeded[2])
-        {
-            ArrayList<String> tutorialsLore = new ArrayList<>();
-            tutorialsLore.add(Utils.loreText(config.getString(ConfigPaths.TUTORIALS_ITEM_LORE)));
-
+        // Set Tutorials Item
+        if(config.getBoolean(ConfigPaths.TUTORIALS_ITEM_ENABLED)) {
+            ArrayList<String> tutorialsLore = new ArrayList<>(Arrays.asList(Utils.loreText(config.getString(ConfigPaths.TUTORIALS_ITEM_LORE))));
             ItemStack tutorialsItem = Item.create(Material.getMaterial(config.getString(ConfigPaths.TUTORIALS_ITEM_MATERIAL)), ChatColor.AQUA +"" +ChatColor.BOLD +"Tutorials", 1, tutorialsLore);
-            MenuItem tutorials = new MenuItem(iSlotsToBeUsed[iItem], tutorialsItem, player ->
-            {
-                //Open New tutorials menu
-            });
-            menuItems.add(tutorials);
+            getMenu().getSlot(slots[2]).setItem(tutorialsItem);
         }
 
-        //--------------------------------------------
-        //-----------------Toggle nav-----------------
-        //--------------------------------------------
-        ArrayList<String> navLore = new ArrayList<>();
-        navLore.add(Utils.loreText("Click to toggle the navigator in your inventory"));
-        navLore.add(Utils.loreText("You can always open the navigator with /navigator"));
-
-        ItemStack navToggleItem = Item.create(Material.KNOWLEDGE_BOOK, ChatColor.AQUA +"" +ChatColor.BOLD +"Tutorials", 1, navLore);
-        MenuItem toggleNav = new MenuItem(18, navToggleItem, player ->
-        {
-            network.updatePreference(PreferenceType.NavigatorEnabled, player.getUniqueId());
-        });
-        menuItems.add(toggleNav);
-
-        return menuItems;
+        super.setPreviewItems();
     }
 
     @Override
-    protected void setMenuItemsAsync()
-    {
-        setMenuItemsAsyncViaMenuItems(menuItems);
-    }
+    protected void setMenuItemsAsync() {}
 
     @Override
-    protected void setItemClickEventsAsync()
-    {
-        setMenuItemClickEventsAsyncViaMenuItems(menuItems);
+    protected void setItemClickEventsAsync() {
+        int[] slots = getSlots();
+
+        // Set Build Item Click Event
+        if(config.getBoolean(ConfigPaths.BUILD_ITEM_ENABLED)) {
+            getMenu().getSlot(slots[0]).setClickHandler((clickPlayer, clickInformation) -> {
+                boolean overwriteDefault = false;
+                clickPlayer.closeInventory();
+
+                if(config.getBoolean(ConfigPaths.BUILD_ITEM_MESSAGE)){
+                    overwriteDefault = true;
+                    clickPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString(ConfigPaths.BUILD_ITEM_MESSAGE)));
+                }
+                if(config.getBoolean(ConfigPaths.BUILD_ITEM_COMMAND)){
+                    overwriteDefault = true;
+                    clickPlayer.performCommand(config.getString(ConfigPaths.BUILD_ITEM_COMMAND));
+                }
+                if(!overwriteDefault)
+                    new BuildMenu(clickPlayer);
+            });
+        }
+
+        // Set Explore Item Click Event
+        if(config.getBoolean(ConfigPaths.EXPLORE_ITEM_ENABLED)) {
+            getMenu().getSlot(slots[1]).setClickHandler((clickPlayer, clickInformation) -> {
+                boolean overwriteDefault = false;
+                clickPlayer.closeInventory();
+
+                if(config.getBoolean(ConfigPaths.EXPLORE_ITEM_MESSAGE)){
+                    overwriteDefault = true;
+                    clickPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString(ConfigPaths.EXPLORE_ITEM_MESSAGE)));
+                }
+                if(config.getBoolean(ConfigPaths.EXPLORE_ITEM_COMMAND)){
+                    overwriteDefault = true;
+                    clickPlayer.performCommand(config.getString(ConfigPaths.EXPLORE_ITEM_COMMAND));
+                }
+                if(!overwriteDefault)
+                    new ExploreMenu(clickPlayer);
+            });
+        }
+
+        // Set Tutorials Item Click Event
+        if(config.getBoolean(ConfigPaths.TUTORIALS_ITEM_ENABLED)) {
+            getMenu().getSlot(slots[2]).setClickHandler((clickPlayer, clickInformation) -> {
+                boolean overwriteDefault = false;
+                clickPlayer.closeInventory();
+
+                if(config.getBoolean(ConfigPaths.TUTORIALS_ITEM_MESSAGE)){
+                    overwriteDefault = true;
+                    clickPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString(ConfigPaths.TUTORIALS_ITEM_MESSAGE)));
+                }
+                if(config.getBoolean(ConfigPaths.TUTORIALS_ITEM_COMMAND)){
+                    overwriteDefault = true;
+                    clickPlayer.performCommand(config.getString(ConfigPaths.TUTORIALS_ITEM_COMMAND));
+                }
+                if(!overwriteDefault)
+                    new TutorialsMenu(clickPlayer);
+            });
+        }
     }
 
     @Override
     protected Mask getMask()
     {
-        return null;
+        return BinaryMask.builder(getMenu())
+                .item(new ItemBuilder(Material.STAINED_GLASS_PANE, 1, (byte) 7).setName(" ").build())
+                .pattern("111111111")
+                .pattern("110101011")
+                .pattern("111111111")
+                .build();
+    }
+
+
+
+    private int[] getSlots() {
+        int[] slots = new int[3];
+
+        boolean buildEnabled = config.getBoolean(ConfigPaths.BUILD_ITEM_ENABLED);
+        boolean exploreEnabled = config.getBoolean(ConfigPaths.EXPLORE_ITEM_ENABLED);
+        boolean tutorialsEnabled = config.getBoolean(ConfigPaths.TUTORIALS_ITEM_ENABLED);
+
+        int buildSlot = 11;
+        int exploreSlot = 13;
+        int tutorialsSlot = 15;
+
+        int enabledItemCount = (buildEnabled ? 1 : 0) + (exploreEnabled ? 1 : 0) + (tutorialsEnabled ? 1 : 0);
+
+        // Depending on how many items are enabled, set the slots to the correct positions
+        if (enabledItemCount == 1) {
+            if (buildEnabled)
+                buildSlot = 13;
+            else if (exploreEnabled)
+                exploreSlot = 13;
+            else if (tutorialsEnabled)
+                tutorialsSlot = 13;
+
+        } else if (enabledItemCount == 2) {
+            if (buildEnabled && exploreEnabled)
+                exploreSlot = 15;
+            else if (exploreEnabled && tutorialsEnabled)
+                exploreSlot = 11;
+
+        }else if(enabledItemCount == 0 || enabledItemCount > 3)
+            return null;
+
+        slots[0] = buildSlot;
+        slots[1] = exploreSlot;
+        slots[2] = tutorialsSlot;
+
+        return slots;
     }
 }

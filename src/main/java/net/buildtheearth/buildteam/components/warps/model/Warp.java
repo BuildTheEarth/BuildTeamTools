85e79daa40749cfd6db07bce384368d720d89fa9
frikandelworst <66461508+frikandelworst@users.noreply.github.com>
2023-09-27 10:34:04 +0200

    Update Warp.java POJO

--- src/main/java/net/buildtheearth/buildteam/components/warps/model/Warp.java
+++ src/main/java/net/buildtheearth/buildteam/components/warps/model/Warp.java
@@ -2,21 +2,30 @@ package net.buildtheearth.buildteam.components.warps.model;
 
 import java.util.UUID;
 
+
 public class Warp {
 
-    private final UUID key;
+    private final String key;
     private final String countryCode;
+
     private final double lat;
     private final double lon;
 
-    public Warp(String countryCode, double lat, double lon) {
-        this.key = UUID.randomUUID();
+    private final double pitch;
+    private final double yaw;
+
+    public Warp(String key, String countryCode, double lat, double lon, double pitch, double yaw) {
+        this.key = key;
         this.countryCode = countryCode;
+
         this.lat = lat;
         this.lon = lon;
+
+        this.pitch = pitch;
+        this.yaw = yaw;
     }
 
-    public UUID getKey() {
+    public String getKey() {
         return key;
     }
 
@@ -30,5 +39,13 @@ public class Warp {
 
     public double getLon() {
         return lon;
+    }
+
+    public double getPitch() {
+        return pitch;
+    }
+
+    public double getYaw() {
+        return yaw;
     }
 }

2b817f735781b9e5c0df87fb164fd10697f10140
GrJo <gregor.jochmann@uni-dortmund.de>
2023-11-17 14:00:26 +0100

    Player teleport to location
    use block Metadata to store Player name on CommandBlock, Teleport to destination

--- src/main/java/net/buildtheearth/modules/kml/KmlCommand.java
+++ src/main/java/net/buildtheearth/modules/kml/KmlCommand.java
@@ -19,6 +19,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.BlockCommandSender;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.material.MaterialData;
 
 import de.micromata.opengis.kml.v_2_2_0.Coordinate;
@@ -30,20 +31,30 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.CommandBlock;
 
+import org.bukkit.metadata.FixedMetadataValue;
+
+
 public class KmlCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String cmdLabel, String[] args) {
         if (sender instanceof BlockCommandSender){
+            Block senderBlock = ((BlockCommandSender)sender).getBlock();
+
+            //read metadata to get player name/ID
+            BlockState blockState = senderBlock.getState();
+            String playerName = blockState.getMetadata("kmlPlayerName").get(0).asString();
+            Player player = Bukkit.getServer().getPlayer(playerName);
+
             Bukkit.getServer().broadcastMessage(
-                    String.format("----SERVER broadcast: received /kml command from commandblock with %d arguments----", args.length)); 
-            //TODO find player who created the block?
+                    String.format("----SERVER broadcast: received /kml command from commandblock with %d arguments. Player from metadata: %s----", args.length, playerName)); 
 
             //parse kml
             KmlParser parser = new KmlParser();
             List<LineString> geoLines = parser.extractLinestrings(String.join(" ", args));
 
-            World world = ((BlockCommandSender)sender).getBlock().getWorld();
+            World world = senderBlock.getWorld();
             List<List<Location>> mcLines = convertToMC(geoLines, world);
+            Location tpLoc = null;
 
             for (List<Location> polyline : mcLines)
             {
@@ -55,10 +66,21 @@ public class KmlCommand implements CommandExecutor {
                     LineRasterization.fillLineWithBlocks(polyline.get(i-1), polyline.get(i), world, Material.GOLD_BLOCK);
                     //set only single block
                     //world.getBlockAt(loc).setType(Material.GOLD_BLOCK);
+                    if (tpLoc == null)
+                    {
+                        tpLoc = polyline.get(0);
+                    }
                 }
                 
             }
-            
+
+            //Teleport player to first location
+
+            if (tpLoc != null && player != null)
+            {
+                player.teleport(tpLoc, TeleportCause.COMMAND);
+                player.sendMessage("imported KML data and teleported to position.");
+            }
 
             //TODO destroy the block
             return true;
@@ -74,25 +96,28 @@ public class KmlCommand implements CommandExecutor {
 
         //The command either creates a command-block at the player location
         //command handling will handled server-side with CommandBlock as CommandSender
-        Block targetedBlock = p.getTargetBlock(null, 10);
-        if (targetedBlock == null || targetedBlock.getType() != Material.COMMAND)
-        {
-            //spawn a command block at the player location
-            Location blockLocation = p.getLocation().add(0, 0, 0);
-            
-            World world = blockLocation.getWorld();
-            Block block = world.getBlockAt(blockLocation);
 
-            block.setType(Material.COMMAND);    
 
-            //for now, user has to manually set the command to "auto" to get it immediately triggered on confirm
-            CommandBlock cmdBlock = (CommandBlock) block.getState();
-            cmdBlock.setCommand("/kml "); //ready to paste kml content
-            //TODO maybe with blocktype selection as argument
-            cmdBlock.update();
-            p.sendMessage("§cCommand block created. Please open the GUI, paste the KML content, set it to 'always on' and confirm");
-            
-        } 
+        //spawn a command block at the player location
+        Location blockLocation = p.getLocation().add(0, 0, 0);
+        
+        World world = blockLocation.getWorld();
+        Block block = world.getBlockAt(blockLocation);
+
+        block.setType(Material.COMMAND);    
+        
+        //for now, user has to manually set the command to "auto" to get it immediately triggered on confirm
+        CommandBlock cmdBlock = (CommandBlock) block.getState();
+        
+        cmdBlock.setCommand("/kml "); //ready to paste kml content
+        cmdBlock.setMetadata("kmlPlayerName", new FixedMetadataValue(Main.instance, p.getName()));
+        cmdBlock.setMetadata("kmlPlayerID", new FixedMetadataValue(Main.instance, p.getUniqueId()));
+        
+        //TODO maybe with blocktype selection as argument
+        cmdBlock.update();
+        p.sendMessage("§cCommand block created. Please open the GUI, paste the KML content, set it to 'always on' and confirm");
+        
+    
       
         //sendHelp(p);
         return true;

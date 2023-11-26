2906dd6c7ebaafa0e4d5d75eb30e2b06174e6337
GrJo <gregor.jochmann@uni-dortmund.de>
2023-11-26 19:05:47 +0100

    javadoc comment for geometricutils returned height
    updated javadoc comments to highlight/explain returned elevation hight from GeometricUtils-functions

--- src/main/java/net/buildtheearth/modules/utils/GeometricUtils.java
+++ src/main/java/net/buildtheearth/modules/utils/GeometricUtils.java
@@ -11,14 +11,20 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class GeometricUtils {
     /**
-     * Creates a minecraft location object for the specified coordinates, yaw and pitch from the BTE projection.. Height is extracted from the world.
+     * Creates a minecraft location object for the specified coordinates, yaw and pitch from the BTE projection.
+     * Height is extracted from the world.
+     * <p>
+     * Note: Height returned is actually terrain elevation +2. This is because this method internally uses
+     * Bukkits @see World::getHighestBlockYAt() already returns elevation+1, and this method deliberately 
+     * adds one to the location elevation on top.
+     * </p
      * The world is extracted from the server config's "earth world". If no earth world is specified then the height defaults to 64
      * and the world is nullified.
      *
      * @param coordinates Latitude and longitude of the location
      * @param yaw        Player's yaw
      * @param pitch      Player's pitch
-     * @return A bukkit location matching the coordinates, yaw and pitch specified.
+     * @return A bukkit location matching the coordinates, yaw and pitch specified. Height is terrain elevation +2.
      */
     public static Location getLocationFromCoordinatesYawPitch(LatLng coordinates, float yaw, float pitch) {
         Airocean projection = new ModifiedAirocean();
@@ -42,12 +48,18 @@ public class GeometricUtils {
     }
 
     /**
-     * Creates a minecraft location object for the specified coordinates from the BTE projection. Height is extracted from the world.
+     * Creates a minecraft location object for the specified coordinates from the BTE projection.
+     * Height is extracted from the world.
+     * <p>
+     * Note: Height returned is actually terrain elevation +2. This is because this method internally uses
+     * Bukkits @see World::getHighestBlockYAt() already returns elevation+1, and this method deliberately 
+     * adds one to the location elevation on top.
+     * </p
      * The world is extracted from the server config's "earth world". If no earth world is specified then the height defaults to 64
      * and the world is nullified.
      *
      * @param coordinates Latitude and longitude of the location
-     * @return A bukkit location matching the coordinates
+     * @return A bukkit location matching the coordinates. Height is terrain elevation +2.
      */
     public static Location getLocationFromCoordinates(LatLng coordinates) {
         Airocean projection = new ModifiedAirocean();

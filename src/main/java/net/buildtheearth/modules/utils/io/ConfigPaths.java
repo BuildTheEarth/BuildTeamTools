f2ef734e9757eed96f295cc1637d1f7315d4849f
MineFact <minefact.minecraft@gmail.com>
2024-01-05 15:31:56 +0100

    fixed config to upgrade properly

--- src/main/java/net/buildtheearth/modules/utils/io/ConfigPaths.java
+++ src/main/java/net/buildtheearth/modules/utils/io/ConfigPaths.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- *  Copyright © 2023, Alps BTE <bte.atchli@gmail.com>
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
 package net.buildtheearth.modules.utils.io;
 
 public abstract class ConfigPaths {
@@ -35,18 +12,18 @@ public abstract class ConfigPaths {
     // Navigator.Item
     private static final String NAVIGATOR_ITEM = "navigator-hotbar-item.";
 
-    public static final String NAVIGATOR_ITEM_ENABLED = NAVIGATOR_ITEM + "enabled";
-    public static final String NAVIGATOR_ITEM_SLOT = NAVIGATOR_ITEM + "slot";
+    public static final String NAVIGATOR_ITEM_ENABLED = NAVIGATOR_ITEM + "nav-enabled";
+    public static final String NAVIGATOR_ITEM_SLOT = NAVIGATOR_ITEM + "nav-slot";
 
     // Navigator.MainMenuItems
-    private static final String NAVIGATOR_MAIN_MENU = "main_menu_items.";
+    private static final String NAVIGATOR_MAIN_MENU = "main-menu-items.";
     private static final String BUILD_ITEM = NAVIGATOR_MAIN_MENU + "build-item.";
-    public static final String BUILD_ITEM_ENABLED = BUILD_ITEM + "enabled";
-    public static final String BUILD_ITEM_ACTION = BUILD_ITEM + "action";
+    public static final String BUILD_ITEM_ENABLED = BUILD_ITEM + "build-enabled";
+    public static final String BUILD_ITEM_ACTION = BUILD_ITEM + "build-action";
 
     private static final String TUTORIALS_ITEM = NAVIGATOR_MAIN_MENU + "tutorial-item.";
-    public static final String TUTORIALS_ITEM_ENABLED = TUTORIALS_ITEM + "enabled";
-    public static final String TUTORIALS_ITEM_ACTION = TUTORIALS_ITEM + "action";
+    public static final String TUTORIALS_ITEM_ENABLED = TUTORIALS_ITEM + "tutorial-enabled";
+    public static final String TUTORIALS_ITEM_ACTION = TUTORIALS_ITEM + "tutorial-action";
 
 
 

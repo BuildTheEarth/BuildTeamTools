566746dbc660e3120449baadda8cafd9535750b6
frikandelworst <kyanvde@gmail.com>
2024-01-14 13:23:23 +0100

    Changed the OSM api request to be more consistent (geocodejson)

--- src/main/java/net/buildtheearth/modules/network/api/OpenStreetMapAPI.java
+++ src/main/java/net/buildtheearth/modules/network/api/OpenStreetMapAPI.java
@@ -2,6 +2,7 @@ package net.buildtheearth.modules.network.api;
 
 import net.buildtheearth.modules.utils.APIUtil;
 import net.buildtheearth.modules.utils.ChatHelper;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
@@ -11,29 +12,59 @@ public class OpenStreetMapAPI extends API {
 
     /**
      * @param coordinates The latitude & longitude coordinates to get the country, region & city/town from
-     * @return A String array with [0] = country, [1] = region & [2] = city/town
+     * @return A String array with [0] = country, [1] = region, [2] = city/town & [3] = cca2CountryCode  region & city/town can be countryName
      */
     public static CompletableFuture<String[]> getCountryAndSubRegionsFromLocationAsync(double[] coordinates) {
         System.out.println("coordinates = " + coordinates[0] + " " + coordinates[1]);
         CompletableFuture<String[]> future = new CompletableFuture<>();
-        String url = "https://nominatim.openstreetmap.org/reverse?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&zoom=10&format=jsonv2";
+        String url = "https://nominatim.openstreetmap.org/reverse?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&zoom=10&format=geocodejson&accept-language=en";
 
         API.getAsync(url, new API.ApiResponseCallback() {
             @Override
             public void onResponse(String response) {
                 JSONObject jsonObject = APIUtil.createJSONObject(response);
-                JSONObject addressObject = (JSONObject) jsonObject.get("address");
 
-                String countryName = (String) addressObject.get("country");
-                String countryCode = (String) addressObject.get("country_code"); //cca2
-                String subRegion = (String) addressObject.get("region");
-                String city = (String) addressObject.get("city");
+                JSONArray featuresArray = (JSONArray) jsonObject.get("features");
+                JSONObject featuresObject = (JSONObject) featuresArray.get(0);
 
-                if (city == null) {
-                    city = addressObject.get("town").toString();
+                JSONObject propertiesObject = (JSONObject) featuresObject.get("properties");
+                JSONObject geoCodingObject = (JSONObject) propertiesObject.get("geocoding");
+
+                String countryCodeCca2 = (String) geoCodingObject.get("country_code");
+                String countryName = (String) geoCodingObject.get("country");
+
+                JSONObject adminObject = (JSONObject) geoCodingObject.get("admin");
+
+                String subRegion = null;
+                byte exitLevel = 0;
+                for(byte i = 6; i < 11; i++) {
+                    if(adminObject.get("level"+i) != null) {
+                        subRegion = (String) adminObject.get("level"+i);
+                        exitLevel = i;
+                        break;
+                    }
                 }
 
-                future.complete(new String[]{countryName, subRegion, city, countryCode});
+                String city = null;
+                for(byte i = (byte) (exitLevel+1); i < 11; i++) {
+                    if(adminObject.get("level"+i) != null) {
+                        city = (String) adminObject.get("level"+i);
+                        break;
+                    }
+                }
+
+                if(subRegion == null) {
+                    subRegion = countryName;
+                    city = countryName;
+                }
+
+                if(city == null) {
+                    city = countryName;
+                }
+
+
+
+                future.complete(new String[]{countryName, subRegion, city, countryCodeCca2});
             }
 
             @Override

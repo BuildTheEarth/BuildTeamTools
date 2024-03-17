71dc4abd7c2b570a34ef7f0e671ad1659f075bde
GrJo <gregor.jochmann@uni-dortmund.de>
2024-03-17 22:13:36 +0100

    Functional/loading PSModule with AlpsLib
    add dependency and setup code

--- src/main/java/net/buildtheearth/modules/plotsystem/PlotSystemModule.java
+++ src/main/java/net/buildtheearth/modules/plotsystem/PlotSystemModule.java
@@ -1,5 +1,16 @@
 package net.buildtheearth.modules.plotsystem;
 
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+
+import com.alpsbte.alpslib.libpsterra.core.Connection;
+import com.alpsbte.alpslib.libpsterra.core.PSTerraSetup;
+import com.alpsbte.alpslib.libpsterra.core.config.ConfigManager;
+import com.alpsbte.alpslib.libpsterra.core.plotsystem.PlotCreator;
+import com.alpsbte.alpslib.libpsterra.core.plotsystem.PlotPaster;
+
+import net.buildtheearth.BuildTeamTools;
 import net.buildtheearth.modules.Module;
 import net.buildtheearth.modules.common.CommonModule;
 import net.buildtheearth.modules.plotsystem.commands.PlotSystemTerraCommand;
@@ -8,6 +19,18 @@ public class PlotSystemModule extends Module {
 
 
     private static PlotSystemModule instance = null;
+
+    private ConfigManager configManager;
+    private PlotPaster plotPaster;
+
+    private boolean pluginEnabled = false;
+    public String version;
+    public String newVersion;
+    public boolean updateInstalled = false;
+
+
+    private Connection connection;
+    private PlotCreator plotCreator;
 
     public PlotSystemModule() {
         super("PlotSystem");
@@ -30,11 +53,35 @@ public class PlotSystemModule extends Module {
             shutdown("§cHeadDatabase is not installed.");
             return;
         }
+        BuildTeamTools plugin = BuildTeamTools.getInstance();
+        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog"); // Disable Logging
 
-        // TODO add onEnable logic here
+        version = plugin.getDescription().getVersion();
+
+        try {
+            PSTerraSetup setup = PSTerraSetup.setupPlugin(plugin, version);
+            this.connection = setup.connection;
+            this.plotCreator = setup.plotCreator;
+            this.plotPaster = setup.plotPaster;
+            this.configManager = setup.configManager;
+        } catch (Exception ex) {
+            Bukkit.getConsoleSender().sendMessage("Error setting up PlotSystemTerra: " + ex.getMessage());
+            Bukkit.getLogger().log(Level.SEVERE, ex.getMessage(), ex);
+
+            shutdown("Error setting up PlotSystemTerra from AlpsLib.");
+            return;
+        }
+
+
+
+        
+
+        Bukkit.getConsoleSender().sendMessage("Enabled Plot-System-Terra module of BuildTeamTools.");
+
 
         super.enable();
     }
+
 
     @Override
     public void registerCommands() {
@@ -43,4 +90,6 @@ public class PlotSystemModule extends Module {
 
     @Override
     public void registerListeners() {}
+
+
 }

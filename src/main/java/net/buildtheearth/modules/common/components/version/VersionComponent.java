b44d425b6450554b555f8b8b89cd557c341bf675
MineFact <minefact.minecraft@gmail.com>
2025-04-24 21:51:07 +0200

    fixes #33

--- src/main/java/net/buildtheearth/modules/common/components/version/VersionComponent.java
+++ src/main/java/net/buildtheearth/modules/common/components/version/VersionComponent.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import net.buildtheearth.modules.ModuleComponent;
 import org.bukkit.Bukkit;
 
+import java.util.Arrays;
+
 public class VersionComponent extends ModuleComponent {
 
 
@@ -12,8 +14,13 @@ public class VersionComponent extends ModuleComponent {
     }
 
     public Version getVersion() {
-        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
-        return Version.valueOf(version);
+        String rawVersion = Bukkit.getBukkitVersion().split("-")[0]; // e.g., "1.21.4"
+        try {
+            return Version.fromBukkitVersion(rawVersion);
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalStateException("Unsupported server version: " + rawVersion, e);
+        }
     }
 
     public boolean is_1_20() {
@@ -24,34 +31,45 @@ public class VersionComponent extends ModuleComponent {
         return getVersion().equals(Version.v1_12_R1);
     }
 
-    enum Version{
-        v1_12_R1("1.12", 12),
-        v1_13_R1("1.13", 13),
-        v1_13_R2("1.13.2", 13),
-        v1_14_R1("1.14.4", 14),
-        v1_15_R1("1.15.2", 15),
-        v1_16_R1("1.16.1", 16),
-        v1_16_R2("1.16.3", 16),
-        v1_16_R3("1.16.5", 16),
-        v1_17_R1("1.17.1", 17),
-        v1_18_R1("1.18.1", 18),
-        v1_18_R2("1.18.2", 18),
-        v1_19_R1("1.19.2", 19),
-        v1_19_R2("1.19.3", 19),
-        v1_19_R3("1.19.4", 19),
-        v1_20_R1("1.20.1", 20),
-        v1_20_R2("1.20.2", 20),
-        v1_20_R3("1.20.4", 20);
+    @Getter
+    public enum Version {
+        v1_12_R1(12, "1.12", "1.12.1", "1.12.2"),
+        v1_13_R1(13, "1.13", "1.13.1"),
+        v1_13_R2(13, "1.13.2"),
+        v1_14_R1(14, "1.14", "1.14.1", "1.14.2", "1.14.3", "1.14.4"),
+        v1_15_R1(15, "1.15", "1.15.1", "1.15.2"),
+        v1_16_R1(16, "1.16", "1.16.1"),
+        v1_16_R2(16, "1.16.2", "1.16.3"),
+        v1_16_R3(16, "1.16.4", "1.16.5"),
+        v1_17_R1(17, "1.17", "1.17.1"),
+        v1_18_R1(18, "1.18", "1.18.1"),
+        v1_18_R2(18, "1.18.2"),
+        v1_19_R1(19, "1.19", "1.19.1", "1.19.2"),
+        v1_19_R2(19, "1.19.3"),
+        v1_19_R3(19, "1.19.4"),
+        v1_20_R1(20, "1.20", "1.20.1"),
+        v1_20_R2(20, "1.20.2", "1.20.3"),
+        v1_20_R3(20, "1.20.4"),
+        v1_21_R1(21, "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4");
 
-        @Getter
-        private final String bukkitVersion;
-
-        @Getter
         private final int xseriesVersion;
+        private final String[] bukkitVersions;
 
-        Version(String bukkitVersion, int xseriesVersion){
-            this.bukkitVersion = bukkitVersion;
+        Version(int xseriesVersion, String... bukkitVersions) {
             this.xseriesVersion = xseriesVersion;
+            this.bukkitVersions = bukkitVersions;
+        }
+
+        public String getLatestBukkitVersion() {
+            return bukkitVersions[bukkitVersions.length - 1];
+        }
+
+        public static Version fromBukkitVersion(String version) {
+            for (Version v : values())
+                if (Arrays.asList(v.bukkitVersions).contains(version))
+                    return v;
+
+            throw new IllegalArgumentException("Unsupported Bukkit version: " + version);
         }
     }
 }

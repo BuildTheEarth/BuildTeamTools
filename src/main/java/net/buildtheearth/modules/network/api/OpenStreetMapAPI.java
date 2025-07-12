4cb0b7da6e4322c832c6640ddddaac6ae294cebb
Zoriot <zoriot@proton.me>
2025-07-13 00:33:07 +0200

    fix(network): 🐛 change from nominatim to photon api for osm data
    It's only a short-term solution. Long term we should use smt local because of speed & rate limits.
    Nominatim just blocked my server :c.

--- src/main/java/net/buildtheearth/modules/network/api/OpenStreetMapAPI.java
+++ src/main/java/net/buildtheearth/modules/network/api/OpenStreetMapAPI.java
@@ -1,5 +1,7 @@
 package net.buildtheearth.modules.network.api;
 
+import net.buildtheearth.utils.ChatHelper;
+import org.jetbrains.annotations.NotNull;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -12,23 +14,25 @@ public class OpenStreetMapAPI extends API {
      * @param coordinates The latitude & longitude coordinates to get the country, region & city/town from
      * @return The country name and country code belonging to this location
      */
-    public static CompletableFuture<String[]> getCountryFromLocationAsync(double[] coordinates) {
+    public static @NotNull CompletableFuture<String[]> getCountryFromLocationAsync(double @NotNull [] coordinates) {
         CompletableFuture<String[]> future = new CompletableFuture<>();
-        String url = "https://nominatim.openstreetmap.org/reverse?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&zoom=10&format=geocodejson&accept-language=en";
+        String url = "https://photon.komoot.io/reverse?lon=" + coordinates[1] + "&lat=" + coordinates[0] + "&lang=en";
+
+        ChatHelper.logDebug("Requesting country from location: %s", url);
 
         API.getAsync(url, new API.ApiResponseCallback() {
             @Override
             public void onResponse(String response) {
                 JSONObject jsonObject = API.createJSONObject(response);
 
-                JSONArray featuresArray = (JSONArray) jsonObject.get("features");
-                JSONObject featuresObject = (JSONObject) featuresArray.get(0);
+                ChatHelper.logDebug("Response from OpenStreetMap: %s", jsonObject);
+
+                JSONObject featuresObject = (JSONObject) ((JSONArray) jsonObject.get("features")).get(0);
 
                 JSONObject propertiesObject = (JSONObject) featuresObject.get("properties");
-                JSONObject geoCodingObject = (JSONObject) propertiesObject.get("geocoding");
 
-                String countryCodeCca2 = (String) geoCodingObject.get("country_code");
-                String countryName = (String) geoCodingObject.get("country");
+                String countryCodeCca2 = (String) propertiesObject.get("countrycode");
+                String countryName = (String) propertiesObject.get("country");
 
                 future.complete(new String[]{countryName, countryCodeCca2});
             }

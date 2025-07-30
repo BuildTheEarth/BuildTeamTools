9a8d5a6d08aad668fea8398c336cdf56b4974336
Zoriot <zoriot@proton.me>
2025-07-30 14:51:19 +0200

    [WIP] feat(navigator/tpll): ✨ use cookie for server-transfer-tpll's

--- src/main/java/net/buildtheearth/modules/navigation/components/tpll/listeners/TpllJoinListener.java
+++ src/main/java/net/buildtheearth/modules/navigation/components/tpll/listeners/TpllJoinListener.java
@@ -1,6 +1,7 @@
 package net.buildtheearth.modules.navigation.components.tpll.listeners;
 
 import net.buildtheearth.modules.navigation.NavigationModule;
+import net.buildtheearth.modules.navigation.components.tpll.TpllComponent;
 import net.buildtheearth.utils.ChatHelper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,5 +13,13 @@ public class TpllJoinListener implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         ChatHelper.logDebug("Player joined. Processing tpll queue for them. Event fired.");
         NavigationModule.getInstance().getTpllComponent().processQueueForPlayer(event.getPlayer());
+        event.getPlayer().retrieveCookie(TpllComponent.TPLL_COOKIE_KEY).thenAcceptAsync(cookie -> {
+            if (cookie != null) {
+                ChatHelper.logDebug("Player has a TPLL cookie, processing it.");
+                NavigationModule.getInstance().getTpllComponent().processCookie(event.getPlayer(), cookie);
+            } else {
+                ChatHelper.logDebug("Player does not have a TPLL cookie.");
+            }
+        });
     }
 }

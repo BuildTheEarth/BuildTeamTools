b1dc7d8825f732af998e62bec5d10d19e71f3c77
Zoriot <zoriot@proton.me>
2026-02-02 19:29:47 +0100

    fix(nav): 🐛 Add other warp group always to warp group selection menu

--- src/main/java/net/buildtheearth/buildteamtools/modules/navigation/components/warps/menu/WarpGroupSelectionMenu.java
+++ src/main/java/net/buildtheearth/buildteamtools/modules/navigation/components/warps/menu/WarpGroupSelectionMenu.java
@@ -1,11 +1,17 @@
 package net.buildtheearth.buildteamtools.modules.navigation.components.warps.menu;
 
+import net.buildtheearth.buildteamtools.modules.navigation.NavUtils;
+import net.buildtheearth.buildteamtools.modules.navigation.components.warps.WarpsComponent;
 import net.buildtheearth.buildteamtools.modules.navigation.components.warps.model.Warp;
 import net.buildtheearth.buildteamtools.modules.navigation.components.warps.model.WarpGroup;
+import net.buildtheearth.buildteamtools.modules.network.NetworkModule;
 import net.buildtheearth.buildteamtools.modules.network.model.BuildTeam;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WarpGroupSelectionMenu extends WarpGroupMenu {
 
@@ -33,6 +39,16 @@ public class WarpGroupSelectionMenu extends WarpGroupMenu {
             clickPlayer.closeInventory();
             new WarpEditMenu(clickPlayer, warp, alreadyExists, true);
         });
+    }
+
+    @Override
+    protected List<?> getSource() {
+        var source = super.getSource().stream().map(l -> (WarpGroup) l).collect(Collectors.toList());
+
+        var otherWarp = WarpsComponent.getOtherWarpGroup(source);
+        if (otherWarp == null) source.add(NavUtils.createOtherWarpGroup(NetworkModule.getInstance().getBuildTeam()));
+
+        return source;
     }
 
     @Override

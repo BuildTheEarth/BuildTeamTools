82e61a9dc65177fb69ff4a33b2ba47aef3220557
Kyan Van den Eynde <66461508+kyanvde@users.noreply.github.com>
2026-04-26 16:22:31 +0200

    fix(nav): Essentials not being installed should be handled gracefully now by the warp migrator.

--- src/main/java/net/buildtheearth/buildteamtools/modules/navigation/components/warps/migrators/EssentialsWarpMigrator.java
+++ src/main/java/net/buildtheearth/buildteamtools/modules/navigation/components/warps/migrators/EssentialsWarpMigrator.java
@@ -1,15 +1,11 @@
 package net.buildtheearth.buildteamtools.modules.navigation.components.warps.migrators;
 
+import com.alpsbte.alpslib.utils.ChatHelper;
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.Warps;
-import com.earth2me.essentials.commands.WarpNotFoundException;
-import net.buildtheearth.buildteamtools.modules.navigation.NavUtils;
 import net.buildtheearth.buildteamtools.modules.navigation.components.warps.WarpsComponent;
 import net.buildtheearth.buildteamtools.modules.navigation.components.warps.model.WarpGroup;
 import net.buildtheearth.buildteamtools.modules.network.NetworkModule;
-import net.ess3.api.IWarps;
-import net.ess3.api.InvalidWorldException;
-
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
@@ -17,15 +13,17 @@ public class  EssentialsWarpMigrator implements IWarpMigrator {
     @Override
     public CompletableFuture<Void> migrate() {
         return CompletableFuture.runAsync(() -> {
-            Essentials essentials = new Essentials();
-            Warps warps = essentials.getWarps();
-            for (String warp : warps.getList()) {
-                try {
+            try {
+                Essentials essentials = (Essentials) Essentials.getProvidingPlugin(Essentials.class);
+                Warps warps = essentials.getWarps();
+                for (String warp : warps.getList()) {
                     WarpGroup group = WarpsComponent.getOtherWarpGroup(Objects.requireNonNull(NetworkModule.getInstance().getBuildTeam()).getWarpGroups());
                     WarpsComponent.createWarp(warps.getWarp(warp), warp, group);
-                } catch (Exception ignored) {
                 }
+            } catch (Exception e) {
+                ChatHelper.logError("An error occurred while migrating the essentials warps!\n(this probably means essentials isn't installed)\n %s", e);
             }
+
         });
     }
 }
